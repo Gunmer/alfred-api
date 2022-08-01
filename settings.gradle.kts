d3ea3202dfae9cff6192f1b9f142bbd3039902c9
Cristiam Sosa <csosaur@gmail.com>
2022-08-01 17:08:22 +0200

    chore: add plugin to check conventional commit format

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "alfred-api"
+
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.18"
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks()
+}
