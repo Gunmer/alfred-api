d38db85ec7bbb09f8a5e63dcbfc3a695d0b6ed25
Cristiam Sosa <csosaur@gmail.com>
2022-08-02 20:15:16 +0200

    test: add JUnit-Extensions library

--- src/test/kotlin/com/gunmer/alfred/test/FixtureGenerator.kt
+++ src/test/kotlin/com/gunmer/alfred/test/FixtureGenerator.kt
@@ -1,19 +1,13 @@
 package com.gunmer.alfred.test
 
-import org.jeasy.random.EasyRandom
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import kotlin.reflect.KClass
 
 object FixtureGenerator {
-    private val generator = EasyRandom()
 
-    fun <T : Any>generateList(type: KClass<T>, nElements: Int) = generator.objects(type.java, nElements).toList()
 
-    fun <T : Any>generate(type: KClass<T>): T = generator.nextObject(type.java)
-
-    fun <T : Any>generateHttpEntity(token: String, body: T?, contentType: MediaType? = MediaType.APPLICATION_JSON): HttpEntity<T> {
+    fun <T : Any> generateHttpEntity(token: String, body: T?, contentType: MediaType? = MediaType.APPLICATION_JSON): HttpEntity<T> {
         val headers = HttpHeaders()
         headers.setBearerAuth(token)
         headers.contentType = contentType
