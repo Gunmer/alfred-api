d52c32d11031772d6cf0c74f3715d9c632b32ac5
Cristiam Sosa <csosaur@gmail.com>
2022-08-02 20:15:16 +0200

    test: add JUnit-Extensions library

--- src/test/kotlin/com/gunmer/alfred/domain/user/usecases/GetUserTest.kt
+++ src/test/kotlin/com/gunmer/alfred/domain/user/usecases/GetUserTest.kt
@@ -1,10 +1,12 @@
 package com.gunmer.alfred.domain.user.usecases
 
+import com.gunmer.alfred.domain.common.DomainExceptions
 import com.gunmer.alfred.domain.user.User
 import com.gunmer.alfred.domain.user.UserRepositoryAdapter
-import com.gunmer.alfred.test.FixtureGenerator
 import com.gunmer.alfred.test.UnitTest
-import org.junit.jupiter.api.Assertions.assertNotNull
+import io.github.glytching.junit.extension.exception.ExpectedException
+import io.github.glytching.junit.extension.random.Random
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
@@ -18,13 +20,23 @@ class GetUserTest {
     @Mock
     lateinit var userRepositoryAdapter: UserRepositoryAdapter
 
+    @Random(excludes = ["family.members"])
+    lateinit var user: User
+
     @Test
-    fun `should return an user when given userId `() {
-        val userId = FixtureGenerator.generate(String::class)
-        whenever(userRepositoryAdapter.find(userId)).thenReturn(FixtureGenerator.generate(User::class))
+    fun `should return an user when given userId`(@Random userId: String) {
+        whenever(userRepositoryAdapter.find(userId)).thenReturn(user)
 
-        val user = useCase(userId)
+        val currentUser = useCase(userId)
 
-        assertNotNull(user)
+        assertEquals(currentUser, user)
+    }
+
+    @Test
+    @ExpectedException(type = DomainExceptions.EntityNotFound::class)
+    fun `should return exception when user not exist`(@Random userId: String) {
+        whenever(userRepositoryAdapter.find(userId)).thenReturn(null)
+
+        useCase(userId)
     }
 }
