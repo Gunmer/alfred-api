e87a5fe88a159df9305d8eb28de1db1ef431ab90
Cristiam Sosa <csosaur@gmail.com>
2022-08-03 18:06:37 +0200

    feat: add current user in family

--- src/main/kotlin/com/gunmer/alfred/db/FamilyDbRepository.kt
+++ src/main/kotlin/com/gunmer/alfred/db/FamilyDbRepository.kt
@@ -1,9 +1,11 @@
 package com.gunmer.alfred.db
 
 import com.gunmer.alfred.db.model.FamilyDao
+import com.gunmer.alfred.db.model.FamilyDao.Companion.convertTo
 import com.gunmer.alfred.db.sources.FamilySource
 import com.gunmer.alfred.domain.family.Family
 import com.gunmer.alfred.domain.family.FamilyRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -13,5 +15,10 @@ class FamilyDbRepository(
     override fun save(family: Family) {
         val familyDao = FamilyDao.convertFrom(family)
         familySource.save(familyDao)
+    }
+
+    override fun findById(familyId: String): Family? {
+        val familyDao = familySource.findByIdOrNull(familyId)
+        return familyDao?.convertTo()
     }
 }
