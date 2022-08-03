ec5db84e0b238bd4862779fdbb04f9561b6afc7e
Cristiam Sosa <csosaur@gmail.com>
2022-08-03 20:12:50 +0200

    feat: retrieve shopping list

--- src/main/kotlin/com/gunmer/alfred/db/ShoppingListDbRepository.kt
+++ src/main/kotlin/com/gunmer/alfred/db/ShoppingListDbRepository.kt
@@ -1,9 +1,11 @@
 package com.gunmer.alfred.db
 
 import com.gunmer.alfred.db.model.ShoppingListDao
+import com.gunmer.alfred.db.model.ShoppingListDao.Companion.convertTo
 import com.gunmer.alfred.db.sources.ShoppingListSource
 import com.gunmer.alfred.domain.shoppinglist.ShoppingList
 import com.gunmer.alfred.domain.shoppinglist.repositories.ShoppingListRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -13,5 +15,10 @@ class ShoppingListDbRepository(
     override fun save(shoppingList: ShoppingList) {
         val shoppingListDao = ShoppingListDao.convertFrom(shoppingList)
         shoppingListSource.save(shoppingListDao)
+    }
+
+    override fun findById(shoppingListId: String): ShoppingList? {
+        val shoppingListDao = shoppingListSource.findByIdOrNull(shoppingListId)
+        return shoppingListDao?.convertTo()
     }
 }
---
+++ src/main/kotlin/com/gunmer/alfred/domain/shoppinglist/converts/ShoppingItemConverter.kt
@@ -0,0 +1,6 @@
+package com.gunmer.alfred.domain.shoppinglist.converts
+
+import com.gunmer.alfred.domain.common.EntityConverter
+import com.gunmer.alfred.domain.shoppinglist.ShoppingItem
+
+interface ShoppingItemConverter<P> : EntityConverter<ShoppingItem, P>
