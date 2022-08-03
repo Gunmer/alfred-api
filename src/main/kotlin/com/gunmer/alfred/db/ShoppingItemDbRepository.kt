377b664b2ccc2291a24f15513cb9eae35a6d851c
Cristiam Sosa <csosaur@gmail.com>
2022-08-04 01:06:31 +0200

    feat: add new item

---
+++ src/main/kotlin/com/gunmer/alfred/db/ShoppingItemDbRepository.kt
@@ -0,0 +1,24 @@
+package com.gunmer.alfred.db
+
+import com.gunmer.alfred.db.model.ShoppingItemDao
+import com.gunmer.alfred.db.model.ShoppingItemDao.Companion.convertTo
+import com.gunmer.alfred.db.sources.ShoppingItemSource
+import com.gunmer.alfred.domain.shoppinglist.ShoppingItem
+import com.gunmer.alfred.domain.shoppinglist.ShoppingItemStatus
+import com.gunmer.alfred.domain.shoppinglist.repositories.ShoppingItemRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+class ShoppingItemDbRepository(
+    val shoppingItemSource: ShoppingItemSource
+) : ShoppingItemRepository {
+    override fun save(shoppingItem: ShoppingItem) {
+        val shoppingItemDao = ShoppingItemDao.convertFrom(shoppingItem)
+        shoppingItemSource.save(shoppingItemDao)
+    }
+
+    override fun findNotArchivedItems(id: String): List<ShoppingItem> {
+        val itemDaos = shoppingItemSource.findAllByShoppingListUuidAndStatusIsNot(id, ShoppingItemStatus.ARCHIVED)
+        return itemDaos.map { it.convertTo() }
+    }
+}
