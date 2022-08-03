1a15d632bd16f98011708d0d605d388cd0722867
Cristiam Sosa <csosaur@gmail.com>
2022-08-03 18:06:37 +0200

    feat: add current user in family

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,4 @@ INSERT INTO families (uuid, name, shopping_list_uuid) VALUES ( '1', 'Sosa Family
 
 INSERT INTO users (uuid, name, family_name, family_uuid) VALUES ('1', 'Cristiam', 'Sosa', '1');
 INSERT INTO users (uuid, name, family_name, family_uuid) VALUES ('2', 'Diego', 'Perez', null);
+INSERT INTO users (uuid, name, family_name, family_uuid) VALUES ('3', 'Mika', 'Sosa', null);
--- src/test/kotlin/com/gunmer/alfred/api/family/FamilyControllerTest.kt
+++ src/test/kotlin/com/gunmer/alfred/api/family/FamilyControllerTest.kt
@@ -3,6 +3,7 @@ package com.gunmer.alfred.api.family
 import com.gunmer.alfred.domain.family.Family
 import com.gunmer.alfred.test.FixtureGenerator
 import com.gunmer.alfred.test.FunctionalTest
+import io.github.glytching.junit.extension.random.Random
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -25,11 +26,31 @@ class FamilyControllerTest {
     }
 
     @Test
-    fun `should return CONFLICT error when current user have  family`() {
+    fun `should return CONFLICT error when current user have family`() {
         val httpEntity = FixtureGenerator.generateHttpEntity("1", null)
 
         val response = template.exchange("/family", HttpMethod.POST, httpEntity, String::class.java)
 
         assertEquals(HttpStatus.CONFLICT, response.statusCode)
+    }
+
+    @Test
+    fun `should return NOT_FOUND when family not exist`(@Random familyId: String) {
+        val httpEntity = FixtureGenerator.generateHttpEntity("1", null)
+
+        val response = template.exchange("/family/$familyId/join", HttpMethod.POST, httpEntity, String::class.java)
+
+        assertEquals(HttpStatus.NOT_FOUND, response.statusCode)
+    }
+
+    @Test
+    fun `should return OK when join in new family`() {
+        val familyId = "1"
+        val httpEntity = FixtureGenerator.generateHttpEntity("3", null)
+
+        val response = template.exchange("/family/$familyId/join", HttpMethod.POST, httpEntity, Family::class.java)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(2, response.body?.members?.size)
     }
 }
