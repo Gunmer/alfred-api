6a9ac35ef48b0e96f1ba3b1b521bbc7bf23de59d
Cristiam Sosa <csosaur@gmail.com>
2022-08-03 18:06:37 +0200

    feat: add current user in family

--- src/main/kotlin/com/gunmer/alfred/api/family/FamilyController.kt
+++ src/main/kotlin/com/gunmer/alfred/api/family/FamilyController.kt
@@ -1,8 +1,10 @@
 package com.gunmer.alfred.api.family
 
 import com.gunmer.alfred.domain.family.usecases.CreateFamily
+import com.gunmer.alfred.domain.family.usecases.JoinInFamily
 import com.gunmer.alfred.domain.user.User
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/family")
 class FamilyController(
-    val createFamily: CreateFamily
+    val createFamily: CreateFamily,
+    val joinInFamily: JoinInFamily
 ) {
     @PostMapping
     fun addNewFamily(@AuthenticationPrincipal user: User) = createFamily(user)
+
+    @PostMapping("/{familyId}/join")
+    fun addNewMember(@AuthenticationPrincipal user: User, @PathVariable familyId: String) = joinInFamily(familyId, user)
 }
--- src/test/kotlin/com/gunmer/alfred/test/FunctionalTest.kt
+++ src/test/kotlin/com/gunmer/alfred/test/FunctionalTest.kt
@@ -1,5 +1,6 @@
 package com.gunmer.alfred.test
 
+import io.github.glytching.junit.extension.random.RandomBeansExtension
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,5 +12,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @Tag("FunctionalTest")
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension::class)
+@ExtendWith(RandomBeansExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 annotation class FunctionalTest
